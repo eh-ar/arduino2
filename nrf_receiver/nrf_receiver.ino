cedbb47f02836d46c9946acd2c78d20f3ff68f6d
soilmapping <soilmappingteam@gmail.com>
2025-02-15 08:55:19 +0330

    nrf module code

--- nrf_receiver/nrf_receiver.ino
+++ nrf_receiver/nrf_receiver.ino
@@ -1,22 +1,26 @@
 #include <SPI.h>
 #include <RF24.h>
 
-RF24 radio(9, 10); // CE, CSN pins
+RF24 radio(9, 10, 4000000); // CE, CSN pins
 
 void setup() {
   Serial.begin(9600);
-  radio.begin();
+  Serial.println("Receiver Initializing...");
+  if (!radio.begin()) {
+    Serial.println("Radio hardware not responding!!");
+    while (1);
+  }
   radio.setChannel(76); // Set the channel
   radio.setPALevel(RF24_PA_MAX); // Set the power level
   radio.openReadingPipe(1, 0xF0F0F0F0E1LL); // Set the address
-  radio.printDetails();
   radio.startListening();
+  Serial.println("Receiver Ready");
 }
 
 void loop() {
   if (radio.available()) {
-    String message;
-    radio.read(&message, sizeof(message));
-    Serial.println(message);
+    char text[32] = {0}; // Create a buffer to hold the received message
+    radio.read(&text, sizeof(text));
+    Serial.println("Received message: " + String(text));
   }
 }
--- nrf_transmitter/nrf_transmitter.ino
+++ nrf_transmitter/nrf_transmitter.ino
@@ -5,15 +5,23 @@ RF24 radio(9, 10); // CE, CSN pins
 
 void setup() {
   Serial.begin(9600);
-  radio.begin();
+  Serial.println("Transmitter Initializing...");
+  if (!radio.begin()) {
+    Serial.println("Radio hardware not responding!!");
+    while (1);
+  }
   radio.setChannel(76); // Set the channel
   radio.setPALevel(RF24_PA_MAX); // Set the power level
   radio.openWritingPipe(0xF0F0F0F0E1LL); // Set the address
-  radio.printDetails();
+  radio.stopListening();
+  Serial.println("Transmitter Ready");
 }
 
 void loop() {
-  String message = "Hello, World!";
-  radio.write(&message, sizeof(message));
+  const char text[] = "Hello, World!";
+  Serial.println("Sending message: " + String(text));
+  if (!radio.write(&text, sizeof(text))) {
+    Serial.println("Transmission failed or timed out");
+  }
   delay(1000);
 }
