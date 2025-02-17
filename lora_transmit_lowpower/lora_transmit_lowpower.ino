e439d81d320f2d10cade425c55435da26834d362
soilmapping <soilmappingteam@gmail.com>
2025-02-17 14:36:44 +0330

    edit

--- lora_transmit_lowpower/lora_transmit_lowpower.ino
+++ lora_transmit_lowpower/lora_transmit_lowpower.ino
@@ -14,13 +14,15 @@ unsigned long timerValue = 0;
 #define DIO0 4
 #define BAND 433E6
 
-#define Rx 3
-#define Tx 2
+#define Rx 2
+#define Tx 3
+
+String ID="FA00002";
 
 int vin = A0;
 int sensorV = A1;
 float vin_m;
-int vin_measure;
+float vin_measure;
 
 SoftwareSerial mySerial(Rx, Tx);
 ModbusMaster node;
@@ -35,19 +37,21 @@ uint16_t data[5];
 String d;
 uint16_t val;
 
+int loopDelay = 20;  //seconds
+
 String readRS485Device(uint8_t deviceAddress, uint8_t st, uint8_t n) {
-  String d = "";
+  String d = ",,,,,";
   node.begin(deviceAddress, mySerial);  // Set the Modbus address and use the SoftwareSerial connection
   //Serial.println("Check RS485");
   result = node.readHoldingRegisters(st, n);
 
   if (result == node.ku8MBSuccess) {
     val = node.getResponseBuffer(0);
-    d = String(deviceAddress) + " " + String(val);
+    d = String(deviceAddress) + "," + String(val);
     for (j = 1; j < n; j++) {
       val = node.getResponseBuffer(j);
       //Serial.println(val);
-      d = d + " " + String(val);
+      d = d + "," + String(val);
     }
   }
   return d;
@@ -58,7 +62,7 @@ void setup() {
   pinMode(NRESET, OUTPUT);
   pinMode(DIO0, INPUT);
   pinMode(vin, INPUT);
-  pinMode(sensorV,  OUTPUT);
+  pinMode(sensorV, OUTPUT);
 
   digitalWrite(sensorV, LOW);
   Serial.begin(115200);
@@ -75,73 +79,77 @@ void setup() {
   Serial.println("LoRa Transmitter");
 
   EEPROM.get(EEPROM_ADDRESS, timerValue);
-  mySerial.begin(9600);
+  mySerial.begin(4800);
   //node.begin(50, mySerial);
   // Set up the watchdog timer to wake up every 8 seconds
-  
 }
 
 
 void loop() {
   Serial.println("-----------------------");
- 
+
   //if (f_wdt) {
   //  f_wdt = false;  // Clear the watchdog timer flag
 
-    // Increment wake-up counter
-    wakeUpCounter++;
-    timerValue += 8;
-    // Check if 8 wake-ups (1 minute) have passed
-    if (wakeUpCounter >= 1) {
-      wakeUpCounter = 0;  // Reset wake-up counter
+  // Increment wake-up counter
+  wakeUpCounter++;
+  timerValue += loopDelay;
+  // Check if 8 wake-ups (1 minute) have passed
+  if (wakeUpCounter >= 1) {
+    wakeUpCounter = 0;  // Reset wake-up counter
 
-      cc++;
-      Serial.print("analog read");
-      int vin_m = analogRead(vin);
-      vin_measure = vin_m * 0.00978;  //* (8/4);
+    cc++;
+    //Serial.println(" Vin");
+    float vin_m = analogRead(vin);
+    
+    vin_measure = vin_m * 0.00978;  //* (8/4);
 
-      String d1 = "";
-      String d2 = "";
+    String d1 = "";
+    String d2 = "";
+    String d3 = "";
+
+    Serial.print("Turn on Sensor");
+    digitalWrite(sensorV, HIGH);
+
+    Serial.print(", rs485 1");
+    d1 = readRS485Device(1, 0, 5);
+    delay(100);
+    d2 = readRS485Device(2, 0, 5);
+    delay(100);
+    d3 = readRS485Device(3, 0, 5);
+    delay(100);
 
 
-      Serial.print(", rs485 1");
-      d2 = readRS485Device(1, 0, 5);
-      delay(100);
-      
-      if (d2 == "") {
-        Serial.print(", rs485 1, try 2");
-        delay(1000);
-        d2 = readRS485Device(1, 0, 6);
-      }
-     
+    Serial.print(", Turn off Sensor");
+    digitalWrite(sensorV, LOW);
 
-      Serial.println(", prepare message");
-      String mmsg = String(timerValue);
+    Serial.println(", prepare message");
+    String mmsg = ID + "," + String(timerValue);
 
-      
-      Serial.print("message: " + mmsg + " " + cc);
-      Serial.print(", voltage: " + String(vin_measure));
-      Serial.print(d1);
-      Serial.println(" " + d2);
 
-      Serial.print("Sending message");
-      LoRa.beginPacket();
-      LoRa.print(mmsg + " - " + cc + " - " + vin_measure + " - " + d1 + " - " + d2);
-      LoRa.endPacket();
-      Serial.println(", message sent");
+    Serial.print("message: " + mmsg + " " + cc);
+    Serial.println(", voltage: " + String(vin_measure));
+    Serial.println(d1 + " " + d2 + " " + d3);
 
-      //delay(3000); // Let serial communication finish
 
-      EEPROM.put(EEPROM_ADDRESS, timerValue);
-      Serial.println("----- ------");
-      //wdt_enable(WDTO_8S);
-      //WDTCSR |= (1 << WDIE);  // Enable interrupt mode
-    }
+    Serial.println("Sending message");
+    LoRa.beginPacket();
+    LoRa.print(mmsg + "," + cc + "," + vin_measure + "," + d1 + "," + d2+ "," + d3);
+    LoRa.endPacket();
+    Serial.println("message sent");
 
-    // Enter sleep mode
-    //sleepNow();
+    //delay(3000); // Let serial communication finish
+
+    EEPROM.put(EEPROM_ADDRESS, timerValue);
+    Serial.println("----- ------");
+    //wdt_enable(WDTO_8S);
+    //WDTCSR |= (1 << WDIE);  // Enable interrupt mode
+  }
+
+  // Enter sleep mode
+  //sleepNow();
   //}
-  delay(8000);
+  delay(loopDelay * 1000);
 }
 
 void sleepNow() {
